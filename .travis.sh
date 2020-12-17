fa619913c1275dc06f04bad576d586a67c59aa0a
Muhammad Ismail Shahzad <57398621+MuhammadIsmailShahzad@users.noreply.github.com>
2020-12-17 15:24:26 +0500

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -12,7 +12,7 @@ elif [ "${1}" == "script" ]; then
     exit 0
 
 elif [ "${1}" == "deploy" ]; then
-    $HOME/bin/travis_ci_operator.sh github-update self master "
+    $HOME/bin/travis_ci_operator.sh github-update self MuhammadIsmailShahzad-patch-1 "
         cd charts_repository &&\
         git checkout ${TRAVIS_BRANCH} && git checkout master -- index.yaml && git commit -m. ;\
         helm package ../ckan --version "${TRAVIS_TAG}" &&\
